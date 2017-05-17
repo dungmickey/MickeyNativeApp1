6131b0da01f38d2b430105d194a60c13586e7349
Dung Tran Thi Thuy <dung.tranthuy@pyramid-consulting.com>
2017-05-17 15:15:51 +0700

    change file Xcode

--- src/network/AuthenticationService.js
+++ src/network/AuthenticationService.js
@@ -5,9 +5,6 @@ import MemCache from '../utlis/MemCache'
 export default class AuthenticationService {
   //Return an promise
   static signin(userCredentials) {
-    //userCredentials = {email : 'duy.le@gmail.com', password: '123456'};
-    //console.log('userCredentials', userCredentials);
-    //userCredentials = {email: 'duy.le@gmail.com', password: '123456'};
     return Api.requestData('POST', 'trainees/login', userCredentials)
     .then(json => {
       MemCache.set(Constants.memcacheKeys.accessToken ,json.token);
